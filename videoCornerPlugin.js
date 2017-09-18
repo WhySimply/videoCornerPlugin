dc824d05e6e01b129b26bf18675dfc0a9e1517e8
Théo Chasseur <tchasseur@monkeyfactory.fr>
2017-09-18 11:07:51 +0200

    maj with doc in js

--- videoCornerPlugin.js
+++ videoCornerPlugin.js
@@ -12,21 +12,21 @@
                 if(!closeVideoBool){
                     setTimeout(function () {
                         $('iframe').attr({
-                            "width": "300px",
-                            "height": "170px"
+                            "width": "300px", // Width of Iframe Youtube Video, change if you want
+                            "height": "170px" // Height of Iframe Youtube Video, change if you want
                         });
                         result.css({
-                            "position": "fixed",
-                            "top": "75px",
-                            "left": "25px",
-                            "width": "320px",
-                            "background-image": "url('https://www.healthlinkbc.ca/hlbc/themes/hlbc/assets/img/menu-close-x.png')",
-                            "background-repeat": "no-repeat",
-                            "background-position": "top right",
-                            "margin": "0",
-                            "padding": "0",
-                            "background-color": "transparent",
-                            "z-index": "20"
+                            "top": "75px", // Position TOP, change if you want
+                            "left": "25px", // Position LEFT, change if you want
+                            "width": "320px", // Width of your video corner, change if you want but + 20px required in relation to width Iframe
+                            "background-image": "url('https://www.healthlinkbc.ca/hlbc/themes/hlbc/assets/img/menu-close-x.png')", // IMG cross for close
+                            "z-index": "20", // For the div to be in the foreground
+                            "position": "fixed", // DON'T CHANGE THIS
+                            "background-repeat": "no-repeat", // DON'T CHANGE THIS
+                            "background-position": "top right", // DON'T CHANGE THIS
+                            "margin": "0", // DON'T CHANGE THIS
+                            "padding": "0", // DON'T CHANGE THIS
+                            "background-color": "transparent" // DON'T CHANGE THIS
                         });
                     }, 0);
                 }else{
@@ -36,8 +36,8 @@
                 console.log('on revient de base');
                 result.removeAttr('style');
                 $('iframe').attr({
-                    "width": originalAttributesWidth,
-                    "height": originalAttributesHeight
+                    "width": originalAttributesWidth, // DON'T CHANGE THIS
+                    "height": originalAttributesHeight // DON'T CHANGE THIS
                 });
             }
         },0);
